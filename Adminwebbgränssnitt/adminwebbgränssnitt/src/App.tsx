4fe47af5be3758bd31c0106da13da19e61e3172e
Abdelrman <absn23@studenr.bth.se>
2024-12-17 19:18:18 +0100

    first app with navbar

---
+++ Adminwebbgränssnitt/adminwebbgränssnitt/src/App.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ Adminwebbgränssnitt/adminwebbgränssnitt/src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import './index.css'
+import { BrowserRouter } from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+     <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
