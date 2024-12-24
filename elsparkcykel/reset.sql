ee6ddbdd271c852720f57e98cbe1c1d8f77bfdff
kamhaal <149787607+kamhaal@users.noreply.github.com>
2024-12-24 08:37:14 +0100

    Update reset.sql

--- elsparkcykel/reset.sql
+++ elsparkcykel/reset.sql
@@ -9,8 +9,6 @@ DROP TABLE IF EXISTS parkingzone;
 DROP TABLE IF EXISTS payment;
 DROP TABLE IF EXISTS scooter;
 DROP TABLE IF EXISTS ride;
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS city;
 DROP TABLE IF EXISTS location;
 
 source ddl.sql;
