15288e41aafbe4393898f1fad2c0c8412ae8ba23
Linn <lich23@student.bth.se>
2025-01-26 20:09:23 +0100

    uppdaterar

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,35 @@
-const { FlatCompat } = require('@eslint/eslintrc');
+const { FlatCompat } = require('@eslint/eslintrc'); // Importera FlatCompat för att stödja äldre extends
 const compat = new FlatCompat({
-    baseDirectory: __dirname, // Endast basmappen behövs
+    baseDirectory: __dirname, // Basen för att hitta konfigurationer
 });
 
 module.exports = [
     {
-        files: ['**/*.{js,jsx,ts,tsx}'], // Specifiera filtyperna direkt här
+        files: ['**/*.{js,jsx,ts,tsx}'], // Matcha alla JavaScript och TypeScript-filer
         languageOptions: {
-            ecmaVersion: 12,
-            sourceType: 'module',
-            parser: '@typescript-eslint/parser',
+            ecmaVersion: 2021, // Stöd för ES2021
+            sourceType: 'module', // ES-moduler
+            parser: require('@typescript-eslint/parser'), // Använd rätt parser
             parserOptions: {
                 ecmaFeatures: {
-                    jsx: true,
+                    jsx: true, // Aktivera JSX-stöd
                 },
             },
         },
         plugins: {
-            react: require('eslint-plugin-react'),
-            '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+            react: require('eslint-plugin-react'), // React-plugin
+            '@typescript-eslint': require('@typescript-eslint/eslint-plugin'), // TypeScript ESLint-plugin
         },
         rules: {
-            'react/react-in-jsx-scope': 'off',
+            'react/react-in-jsx-scope': 'off', // React 17+ behöver inte `React` i scope
         },
         settings: {
             react: {
-                version: 'detect',
+                version: 'detect', // Automatisk React-version
             },
         },
     },
+    // Använd FlatCompat för att inkludera äldre extends
     ...compat.extends('eslint:recommended'),
     ...compat.extends('plugin:react/recommended'),
     ...compat.extends('plugin:@typescript-eslint/recommended'),
