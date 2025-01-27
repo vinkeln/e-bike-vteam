4239cc73788ec447e67e049732c9aa9cb5b7af3e
Kamar <kahj23@student.bth.se>
2025-01-27 11:17:35 +0100

    eslint

--- scooter-rental/eslint.config.js
+++ scooter-rental/eslint.config.js
@@ -1,31 +1,30 @@
-// eslint.config.js
-const reactPlugin = require('eslint-plugin-react');
-
 module.exports = {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
-    languageOptions: {
-        ecmaVersion: 2021,
-        sourceType: "module",
-        globals: {
-            Atomics: "readonly",
-            SharedArrayBuffer: "readonly",
-            process: "readonly",
-            require: "readonly",
-            module: "readonly"
-        }
+    env: {
+      browser: true, // Lägger till webbläsarspecifika globala variabler
+      es2021: true, // Aktiverar ES2021 syntax support
+      node: true // Aktiverar Node.js globala variabler och Node.js scoping
     },
-    plugins: {
-        react: reactPlugin
+    extends: [
+      'eslint:recommended', // Använder ESLint rekommenderade regler
+      'plugin:react/recommended' // Om du använder React, annars ta bort detta
+    ],
+    parserOptions: {
+      ecmaVersion: 12, // Moderna JavaScript-funktioner
+      sourceType: 'module', // Aktiverar användning av ES Modules
+      ecmaFeatures: {
+        jsx: true // Om du använder JSX, annars sätt detta till false
+      }
     },
     rules: {
-        "no-unused-vars": "warn",
-        "eqeqeq": "error",
-        "no-undef": "error",
-        "no-prototype-builtins": "off"
+      'no-unused-vars': 'warn', // Varnar för oanvända variabler
+      'no-console': 'off', // Tillåter användning av console.log, etc.
+      'eqeqeq': ['error', 'always'], // Kräver strikt jämförelse '==='
+      'curly': 'error' // Kräver klammerparenteser för alla kontrollstrukturer
     },
     settings: {
-        react: {
-            version: "detect"
-        }
+      react: {
+        version: 'detect' // Låter ESLint automatiskt upptäcka versionen av React
+      }
     }
-};
+  };
+  
